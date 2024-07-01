9378dcdb9e1c560988710cb0291c1d728c497204
sharma7ay <sharma7ay@gmail.com>
2024-07-01 16:03:37 +0545

    second commit

--- script.js
+++ script.js
@@ -1,1 +1,21 @@
-console.log("hello world")
+// var bottle = "pani"
+// let bottle = 'pani'
+// const bottle = 'pani'
+
+const friend1 = 'pranjal'
+const friend2 = 'anjila'
+const friend3 = 'aryan'
+
+const friend = ['pranjal','anjila','aryan']
+
+const name = 'jp'
+const age = '21'
+const address = 'haatisar'
+
+const me = {
+    name : 'jp',
+    age : '21',
+    address : 'birgunj'
+}
+
+console.log(me)
