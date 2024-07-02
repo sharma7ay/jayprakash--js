a915fd162acf4bbc2284c109476e3da39c0ebb2a
sharma7ay <sharma7ay@gmail.com>
2024-07-02 14:44:11 +0545

    jsday2

---
+++ day2.js
@@ -0,0 +1,100 @@
+// const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday']
+
+// //console.log(days[3])
+
+// //days.push('Thrusday')
+// //days.push()
+// //days.unshift('Hello')
+// //days.shift()
+// //const data = days.slice(1,3)
+// //console.log(data)
+
+// days.splice(3,3,'dai')
+// console.log(days)
+
+// const data = [1,2,3,4,5,6]
+// // output --> [1,'hello',bye','world',5,6]
+// data.splice(1,3,'hello','bye','world')
+// console.log(data)
+
+// const vowels = ['j','a','y']
+// vowels.splice(0,3,10,1,25)
+// //output --> [10,1,25]
+// console.log(vowels)
+
+// const text = 'I am from Nepal'
+// const splittedData = text.split(" ")
+// console.log(splittedData)
+
+// const languages = ['js', 'php','c']
+// //output --> ['js','html','php','c']
+// const newLanguages = languages.slice(0, 1).concat('html', languages.slice(1));
+// console.log(newLanguages);
+
+// const person = {
+//     name : "Jay"
+//     Address : "Birgunj"
+//     Nationality : "Nepal"
+// }
+
+// console.log(person.address)
+// console.log(person['address'])
+// person.age = 22
+// person.haha = "hehe"
+// console.log(person)
+
+// const person = Object.freeze({
+//     name: "Jay"
+//     address: "Birgunj"
+//     nationality: "Nepal"
+// })
+
+// console.log(Object.keys(person))
+// console.log(Object.values(person))
+
+// const datas = {
+//     name : ['Jay','Eli']
+// }
+// datas.name.push('Sanchit')
+// console.log(datas)
+// // //output ['jay','Eli','Sanchit']
+// // //data.name[0] = data.name[0].toLowerCase()
+// // data.name.push('Sanchit')
+// // console.log(data.name)
+
+
+// const text = [
+//     {
+//         name : 'Jay'
+//     },
+//     {
+//         name : 'Eli'
+//     }
+// ]
+// text.push(
+//     {
+//         name : 'Sanchit'
+//     }
+// )
+// console.log(text)
+// //output -->[
+
+// {
+//     name : 'jay'
+// }
+// {
+//     name : 'Eli'
+
+// }
+// {
+//     name : 'sanchit'
+// }
+// ]
+
+
+const text2 = {
+    name : ['Jay','Eli','Sanchit']
+}
+
+text2.name.splice(0,3,1,2,3)
+console.log(text2)
